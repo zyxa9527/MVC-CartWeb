815e307881ea45beec9119bc3a42629e6e6ee29c
zyxa9527 <zyxa9527@gmail.com>
2020-05-18 13:39:51 +0800

    MVC Cart
    MVC 購物車網站

---
+++ MVC購物網站/WebApplication9/Controllers/LoginController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication9.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            ViewBag.ResultMessage = TempData["ResultMessage"];
+            return View();
+        }
+        
+       [HttpPost]
+        public ActionResult Login(Models.User postback)
+        {
+            
+            try
+            {
+                if (Db.CheckUserData(postback.Name, postback.Password))
+                {
+                    Session["LoginName"] = postback.Name;
+                    Session["LoginPassword"] = postback.Password;
+                    TempData["ResultMessage"] = String.Format("{0}登入成功!!" , postback.Name);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    TempData["ResultMessage"] = "帳號或密碼錯誤";
+                    return RedirectToAction("Index","Login");
+                }
+            }
+            catch
+            {
+                return RedirectToAction("Index", "Login");
+                
+            }
+
+        }
+            /// <summary>
+            /// 登出
+            /// </summary>
+            public ActionResult Logout()
+        {
+            Session.Abandon();
+            return RedirectToAction("Index","Login");
+        }
+        public class Db
+        {
+           
+            public static SqlConnection CreateConnection()//64814
+            {
+                SqlConnection con = new SqlConnection("Data Source =192.168.56.1,64814;Initial Catalog =Carts;User ID =zyxa9527;Password = a3727240");
+                return con;
+            }
+           
+            public static bool CheckUserData(string name, string password)
+            {
+                SqlConnection con = Db.CreateConnection();
+                try
+                {
+                   
+                    string strSQL = "select userpassword from [User] where username = '" + name + "' and userpassword = '" + password + "'" ;
+                    SqlCommand cmd = new SqlCommand(strSQL, con);
+                
+                    con.Open();
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                            return true;
+                    }
+                    
+                    return false;
+
+                }
+                catch (Exception ex)
+                {
+                    string error = ex.ToString();
+                    
+                    return false;
+                }
+                finally
+                {
+                    con.Close();
+                }
+                
+            }
+        }
+
+    }
+}
---
+++ MVC購物網站/WebApplication9/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication9.Models
+{
+    public class User
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Password { get; set; }
+
+    }
+}
